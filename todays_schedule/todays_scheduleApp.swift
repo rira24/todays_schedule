0b67476c364586d960b21353a35cb3c4114c5e31
Rithika <rithu427@gmail.com>
2023-07-19 11:23:52 -0500

    Initial Commit

---
+++ todays_schedule/todays_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  todays_scheduleApp.swift
+//  todays_schedule
+//
+//  Created by Scholar on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct todays_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
