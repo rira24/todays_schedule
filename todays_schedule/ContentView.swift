13aae59e5c6af50cfbeefcf4b61400c38f50a545
Rithika <rithu427@gmail.com>
2023-07-21 11:43:45 -0500

    ,.m.,

--- todays_schedule/ContentView.swift
+++ todays_schedule/ContentView.swift
@@ -6,17 +6,58 @@
 //
 
 import SwiftUI
-
+import Foundation
+//dateFormatter.dateFormat = "dd/MM/yyyy"
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+        ZStack {
+            Color(.systemGreen)
+                .ignoresSafeArea()
+            
+            
+            VStack {
+                Spacer().frame(height: 30)
+                Text("Today's Schedule")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.black)
+                   // .font(.custom(name:String, size: 12))
+            
+                
+                ZStack{
+                 
+                        RoundedRectangle(cornerRadius: 20).foregroundColor(Color(.white)).padding()
+                    VStack{
+                        Text("Friday, July 21, 2023").font(.title).fontWeight(.bold).padding()
+                        
+                        Text("9:00am - 9:30am: Wake Up")
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        Text("1:00pm: Demo Day!")
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        Spacer().frame(height: 450)
+                    }
+                }
+
+            }
+            Text("hello")
+            
+            
+            
+        }.background(Rectangle().foregroundColor(Color(.white)))
+    
     }
+    init() {
+                            for familyName in UIFont.familyNames {
+                                print(familyName)
+                               
+                                for fontName in UIFont.fontNames(forFamilyName: familyName){
+                                    print("--\(fontName)")
+                                }
+                            }
+                        }
 }
 
 struct ContentView_Previews: PreviewProvider {
